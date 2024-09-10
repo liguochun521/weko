88633201024325fb21faec6e464dbdc4e3cd0bfe
liguochun521 <li.guochun@za.ivis.co.jp>
2024-09-10 17:56:01 +0900

    postgresバージョンアップ_SQLAlchemy対応_単体テスト実施

--- modules/weko-index-tree/tests/test_tasks.py
+++ modules/weko-index-tree/tests/test_tasks.py
@@ -31,7 +31,15 @@ def test_update_oaiset_setting(i18n_app, client_api, indices, db, users, without
         description='some test description',
         search_pattern='test search'
     )
-    
+     
+    test_set_three = OAISet(
+        id=33,
+        spec='33',
+        name='test_name_33',
+        description='some test description',
+        search_pattern='test search'
+    )
+           
     test_set_three_child = OAISet(
         id=44,
         spec='44',
@@ -47,7 +55,7 @@ def test_update_oaiset_setting(i18n_app, client_api, indices, db, users, without
         description='some test description',
         search_pattern='test search'
     )
-    
+
     login_user_via_session(client=client_api, email=users[3]["email"])
     index_info_one = Indexes.get_path_name([indices['index_non_dict'].id])
     index_info_two = Indexes.get_path_name([indices['index_non_dict_child'].id])
@@ -62,6 +70,7 @@ def test_update_oaiset_setting(i18n_app, client_api, indices, db, users, without
 
     db.session.add(test_set_one)
     db.session.add(test_set_two)
+    db.session.add(test_set_three)
     db.session.add(test_set_three_child)
     db.session.add(test_set_private)
     db.session.commit()
@@ -71,6 +80,7 @@ def test_update_oaiset_setting(i18n_app, client_api, indices, db, users, without
     assert res.name=="testIndexThree"
     assert res.description=="testIndexThree"
     assert res.search_pattern=='path:"33"'
+    assert res.system_created==False    
     assert res.spec=="33"
 
     update_oaiset_setting(index_info_two[0], data_two)
@@ -99,12 +109,34 @@ def test_update_oaiset_setting(i18n_app, client_api, indices, db, users, without
         "id": "33",
         "index_name": "test data"
     }
+
     update_oaiset_setting(None, _data)
     res = OAISet.query.filter_by(id=33).one_or_none()
     assert res.name=="testIndexThree"
     assert res.description=="testIndexThree"
     assert res.search_pattern=='path:"33"'
     assert res.spec=="33"
+ 
+    _data_six = {
+        "public_state": True,
+        "harvest_public_state": True,
+        "parent": 0,
+        "id": 66,
+        "index_name": "testData",
+        "search_pattern": "test search",
+        "system_created": False
+    }
+
+    res = OAISet.query.filter_by(id=66).one_or_none()
+    assert res is None
+
+    update_oaiset_setting(None, _data_six)
+    res = OAISet.query.filter_by(id=66).one_or_none()
+    # assert res is None
+    assert res.name=="testData"
+    assert res.description=="testData"
+    assert res.search_pattern=='path:"66"' 
+    assert res.spec=="66"
 
 
 # def delete_oaiset_setting(id_list):
--- modules/weko-items-ui/tests/test_utils.py
+++ modules/weko-items-ui/tests/test_utils.py
@@ -8153,12 +8153,14 @@ def test__get_max_export_items(app,users):
         with patch("flask_login.utils._get_user", return_value=users[2]["obj"]):
             assert _get_max_export_items() == size
     
-    with patch("flask_sqlalchemy._QueryProperty.__get__") as mock_query:
+    # with patch("flask_sqlalchemy._QueryProperty.__get__") as mock_query:
+    with patch("weko_items_ui.utils.db.session.query", side_effect=Exception("test_error")):
         size = WEKO_ITEMS_UI_MAX_EXPORT_NUM_PER_ROLE[users[2]['obj'].roles[0].name]
         app.config['WEKO_ITEMS_UI_DEFAULT_MAX_EXPORT_NUM'] = 20
-        #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dummy.db"
-        #mock_query.return_value.all.side_effect = Exception("test_error")
-        mock_query.return_value.all.side_effect  = Exception("test_error")
+        
+        # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dummy.db"
+        # mock_query.return_value.all.side_effect = Exception("test_error")
+        # mock_query.return_value.all.side_effect = Exception("test_error")
         with patch("flask_login.utils._get_user", return_value=users[2]["obj"]):
             assert _get_max_export_items() == 20
             
